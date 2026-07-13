2aba26d50e640b684fd876e637b93c958768100c
= <=>
2026-07-13 11:48:43 +0530

    commit 2: Added url and async function to fetch the weather data in script.js

--- front-end/script.js
+++ front-end/script.js
@@ -1,12 +1,29 @@
 let city = document.querySelector("#city")
 let submit = document.querySelector("#submit-city")
 
+async function display_weather(place)
+{
+    let url = `https://goweather.xyz/v2/weather/${place}`
+    try
+    {
+        let data = await fetch(url)
+        let weather_data = data.json()
+        console.log(weather_data)
+        
+    }
+    catch (error)
+    {
+        console.log("Unable to fetch the weather data")
+    }
+}
+
 function select_city()
 {
     place = city.value
     place = place.trim()
     place = place.charAt(0).toUpperCase() + place.slice(1).toLowerCase()
     console.log(place)
+    display_weather(place)
 }
 
 submit.addEventListener("click" , select_city)
