5abdc1d6596391e1acda94195e3c18b664782014
= <=>
2026-07-27 20:05:57 +0530

    Modified allowed host on settings.py

--- back-end/weather_project/settings.py
+++ back-end/weather_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_kv8lxrvhbmryw8_2ev7+6huhy=x$rru)syqh5w6a_$07+acbf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
