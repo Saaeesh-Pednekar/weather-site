fcd93654374a4f551a83d13f9234a6df00c4a9de
= <=>
2026-07-27 22:24:01 +0530

    cors are modified in settings.py

--- back-end/weather_project/settings.py
+++ back-end/weather_project/settings.py
@@ -127,7 +127,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://weather-site-app.onrender.com/",
-    "http://localhost",
-    "http://127.0.0.1",
+    "https://weather-site-app.onrender.com",  # 👈 Your FRONTEND Render URL (No trailing slash)
+    "http://localhost:8080",                  # Common local docker testing port
+    "http://localhost:80",                    # Standard local Nginx port
+    "http://127.0.0.1:8080",
+    "http://127.0.0.1:80",
 ]
