d590706dcf0cc5e75c439f6cb66f5344e4e1ce31
= <=>
2026-07-27 20:32:00 +0530

    Modified url for fetching data from render

--- back-end/weather_project/settings.py
+++ back-end/weather_project/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
+    "https://vercel.app",
     "http://localhost",
     "http://127.0.0.1",
 ]
--- front-end/script.js
+++ front-end/script.js
@@ -40,13 +40,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let submit = document.querySelector("#submit-city")
     let resultBox = document.querySelector("#weather-result")
 
-
+    // 🌟 Step 1: Automatically toggle between Local Testing and Live Render Backend
+    const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+        ? 'http://127.0.0.1:8000'
+        : 'https://weather-site.onrender.com';
 
 
     async function display_weather(place)
     {
         // 🌟 1. Route the network call directly to your Django backend port
-        let url = `http://localhost:8000/weather/api/forecast/?city=${place}`
+        // let url = `http://localhost:8000/weather/api/forecast/?city=${place}`
+        let url = `${API_BASE_URL}/weather/api/forecast/?city=${place}`
         resultBox.innerHTML = `<p>Fetching data for ${place}...</p>`
 
 
